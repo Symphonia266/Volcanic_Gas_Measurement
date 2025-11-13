33080830e7537d0d39e76d54d1bd18e6e2174d5f
Symphonia266 <symphoniea266@gmail.com>
2025-11-14 01:01:00 +0900

    wip: feat measurement model for lidar

--- gas_simulation/lidar_simulation/model.py
+++ gas_simulation/lidar_simulation/model.py
@@ -4,8 +4,10 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt
 from pathlib import Path
+
 from . import package_path
 from ..diffusion_model.diffuse_plume import DiffusePlumeLidar
+from ..diffusion_model.func import gen_fauntainsource
 from ..diffusion_model.pasquill_stable_classfication import (
   classification_label, 
   classification_table, 
@@ -17,36 +19,24 @@ class MeasurementModel:
         self.speed = windspeed
         self.wind_direction = wind_direction
         self.elevation = elevation
-        self.diffusemodel = DiffusePlumeLidar("pasquill", windspeed, wind_direction, elevation, wether=wether, stab_class=stab_class)
-        self.wether = self.diffusemodel.core.wether
-        self.stab_class = self.diffusemodel.core.stab_class
+        self.diffusemodel = DiffusePlumeLidar("pasquill", windspeed, wind_direction, wether=wether, stab_class=stab_class)
+        self.wether = self.diffusemodel.wether
+        self.stab_class = self.diffusemodel.stab_class
 
-        end = 1000
         dR = 5
-        self.lidar_grid = np.arange(0, end, dR)
-        self.gases = pd.DataFrame({"dist": self.lidar_grid})
+        self.distance = np.arange(0, 100, dR)
+        self.x_grid = self.distance * np.cos(np.deg2rad(self.elevation))
+        self.z_grid = self.distance * np.sin(np.deg2rad(self.elevation))
+        self.gases = pd.DataFrame({"dist": self.distance})
 
-        r = 5
-        N = 100
-        source_grid_X, source_grid_Y = np.meshgrid(np.linspace(-r, r, N), np.linspace(-r, r, N))
-        mask = (
-            source_grid_X**2 + source_grid_Y**2
-        ) <= r**2
-        cnt_x = 10
-        cnt_y = -20
-        x_src = source_grid_X[mask] - cnt_x
-        y_src = source_grid_Y[mask] - cnt_y
-        z_src = 0.0
-        q_src = 1 / mask.sum()
-        self.diffusemodel.entry_source(q_src, x_src, y_src, z_src)
-        x = self.diffusemodel.core.sources["x"]
-        y = self.diffusemodel.core.sources["y"]
-        q = self.diffusemodel.core.sources["Q"]
-        plt.figure()
-        plt.scatter(x, y,c=q)
-        plt.show()
-        print(self.diffusemodel.core.sources)
-        self.C = self.diffusemodel.Concentration(x_lidar=self.lidar_grid, time_correction=10*60)
+        x_src, y_src, z_src, q_src = gen_fauntainsource(
+            radius=5, 
+            cnt=(50,-40,0),
+            N_pt=100
+        )
+        H_src = 2
+        self.diffusemodel.entry_source(q_src, x_src, y_src, z_src, H_src)
+        self.C = self.diffusemodel.Concentration(self.x_grid, z=self.z_grid, time_correction=10*60)
     
     def entry_gases(self, gas_name:str, Q, offset):
         self.gases[gas_name] = Q*self.C + offset
@@ -55,13 +45,15 @@ class MeasurementModel:
         fig, ax = plt.subplots(1,1, layout="constrained")
         ax.grid(which="minor", ls="--", c="lightgrey")
         ax.grid(which="major", ls="-", c="darkgrey")
+
         ax2 = ax.twinx()
-        for key in self.gases.columns:
-            ax.scatter(self.lidar_grid, self.gases[key], label=key)
-        x = np.linspace(self.lidar_grid.min(), self.lidar_grid.max(), 1000)
-        ax2.plot(x, self.diffusemodel.Concentration(x_lidar=x, time_correction=10*60), c="darkgrey")
+        for key in self.gases.columns[1:]:
+            ax.scatter(self.distance, self.gases[key], label=key)
+        r = np.linspace(self.distance.min(), self.distance.max(), 1000)
+        ax2.plot(r, self.diffusemodel.Concentration(r, time_correction=10*60), c="darkgrey")
         ax.set_xlabel("distance [m]")
         ax.set_ylabel("concentration [ppm]")
         ax2.set_ylabel("coefficient")
+        ax.legend()
         plt.show(block=False)
         input("ENTER ANY KEY...")
--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import os
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 from matplotlib.colors import LogNorm
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -10,10 +10,10 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from gas_simulation.diffusion_model.diffuse_plume import DiffusePlumeLidar as DP
 from gas_simulation.lidar_simulation.model import MeasurementModel as MM
 
-windspeed = 10
-stab_class = "C"
+windspeed = 2
+stab_class = "A"
 model = MM(windspeed=windspeed, wind_direction=90, elevation=0, stab_class=stab_class)
-model.entry_gases("SO2", 10.0, 0.0)
-model.entry_gases("H2S",  5.0, 0.0)
-model.entry_gases("O3" ,  0.0, 0.005)
+model.entry_gases("SO2", 2.5*1e4, 0.0)
+model.entry_gases("H2S", 1.0*1e4, 0.0)
+model.entry_gases("O3" , 0.0, 0.005)
 model.show_gases()

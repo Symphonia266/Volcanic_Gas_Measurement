42b4428e30bfb349da6740b76911e0da160b2c44
Symphonia <symphoniea266@gmail.com>
2025-12-12 12:33:54 +0900

    feat: Refactor PlumeEnvironment and InstantEnvironment classes; enhance gas handling and plotting functionality

--- gas_simulation/model.py
+++ gas_simulation/model.py
@@ -44,33 +44,37 @@ class Gas:
     cross_section: callable
 
 
-class PlumeEnvironment:
-    def __init__(
-        self, field: Field, source: Source, time: float, gas: dict
-    ):
-        self.field = field
-        self.source = source
-        self.plume_model = PlumeModel(self.field, self.source)
+class InstantEnvironment:
+    def __init__(self, gas: dict, time: float, trig: callable = None):
 
         self.gas_inventory = {k: obj for k, obj in gas.items()}
         self.time = time
         self.aer_absorp_feat = 1.0
+
+        if trig is None:
+            self.trig = lambda x: False
+
+        else:
+            self.trig = trig
+
         print(f"elapsed time(and multiplier time)   : {utils.elapsed_time_str(time)}")
         print(f"multi. coeff. in aerzol absorptance : {self.aer_absorp_feat:.2f}\n")
 
     def number_density_at(self, x, y, z):
-        C = self.plume_model.calc(x, y, z, time=self.time)
-        gas = {
-            name: utils.ppm_to_number_density(obj.offset + obj.Q * (C), z)
-            for name, obj in self.gas_inventory.items()
-        }
-        return gas
+        x = np.atleast_1d(x)
+        n = np.zeros_like(x)
+        n_gas = {}
+        for key, obj in self.gas_inventory.items():
+            mask = self.trig(x)
+            n = np.where(mask, obj.Q, obj.offset)
+            n_gas[key] = utils.ppm_to_number_density(n, z)
+        return n_gas
 
-    def transmittance(self, coord:Coord, wl:float|np.ndarray, *, show=False):
+    def transmittance(self, coord: Coord, wl: float | np.ndarray, *, show=False):
         x_tmp, z_tmp = coord.get_xz(0)
-        distance = np.append(0,     coord.distance)
-        x        = np.append(x_tmp, coord.x       )
-        z        = np.append(z_tmp, coord.z       )
+        distance = np.append(0, coord.distance)
+        x = np.append(x_tmp, coord.x)
+        z = np.append(z_tmp, coord.z)
 
         distance = np.atleast_1d(distance)[:, np.newaxis]
         x = np.atleast_1d(x)[:, np.newaxis]
@@ -92,9 +96,7 @@ class PlumeEnvironment:
         absorptance = absorptance_mol + absorptance_aer + sum(absorptance_gas.values())
 
         intgr = np.cumsum(
-            (absorptance[:-1] + absorptance[1:])
-            * np.diff(distance, axis=0)
-            * 0.5,
+            (absorptance[:-1] + absorptance[1:]) * np.diff(distance, axis=0) * 0.5,
             axis=0,
         )
         transmittance = np.exp(-intgr)
@@ -105,7 +107,7 @@ class PlumeEnvironment:
                 ax.grid(which="minor", ls="--", c="lightgrey")
                 ax.set_xlabel("lidar distance [m]")
                 # ax.set_yscale("log")
-            axes[0].scatter(distance,       absorptance[:, 0])
+            axes[0].scatter(distance, absorptance[:, 0])
             axes[1].scatter(coord.distance, transmittance[:, 0])
             axes[0].set_ylabel("absorptance")
             axes[1].set_ylabel("transmittance")
@@ -114,40 +116,71 @@ class PlumeEnvironment:
             plt.show(block=False)
         return transmittance
 
-    def show_gases(self, coord:Coord):
-        fig = plt.figure()
+    def show_gases(self, coord: Coord):
+        fig, ax = plt.subplots(1, 1, layout="constrained")
+        ax.grid(which="minor", ls="--", c="lightgrey")
+        ax.grid(which="major", ls="-", c="darkgrey")
+
+        gas = self.number_density_at(coord.x, 0, coord.z)
+        for name, n in gas.items():
+            ax.plot(
+                coord.distance,
+                utils.number_density_to_ppm(n, coord.z),
+                marker="o",
+                label=name,
+            )
+        ax.set_xlabel("distance [m]")
+        ax.set_ylabel("concentration [ppm]")
+        ax.set_xlim(coord.x.min(), coord.x.max())
+        ax.set_ylim(0, None)
+        ax.legend()
+
+        plt.show(block=False)
+        # input("ENTER ANY KEY...")
+
+
+class PlumeEnvironment(InstantEnvironment):
+    def __init__(self, gas: dict, time: float, field: Field, source: Source):
+        super().__init__(gas, time)
+        self.field = field
+        self.source = source
+        self.plume_model = PlumeModel(self.field, self.source)
+
+    def number_density_at(self, x, y, z):
+        C = self.plume_model.calc(x, y, z, time=self.time)
+        gas = {
+            name: utils.ppm_to_number_density(obj.offset + obj.Q * (C), z)
+            for name, obj in self.gas_inventory.items()
+        }
+        return gas
+
+    def show_gases(self, coord: Coord):
+        r = np.linspace(coord.distance.min(), coord.distance.max(), 200)
+        x = np.linspace(coord.x.min(), coord.x.max(), 200)
+        z = np.linspace(coord.z.min(), coord.z.max(), 200)
+
+        fig = plt.figure(layout="constrained")
         ax1 = fig.add_subplot(1, 2, 1)
-        ax2 = ax1.twinx()
         ax3 = fig.add_subplot(1, 2, 2)
         ax1.set_axisbelow(True)
-        ax2.set_axisbelow(True)
         ax3.set_axisbelow(True)
         ax1.grid(which="minor", ls="--", c="lightgrey")
         ax1.grid(which="major", ls="-", c="darkgrey")
         ax3.grid(which="minor", ls="--", c="lightgrey")
         ax3.grid(which="major", ls="-", c="darkgrey")
-
         gas = self.number_density_at(coord.x, 0, coord.z)
-        p1 = []
         for name, n in gas.items():
-            p = ax1.scatter(
+            ax1.plot(
                 coord.distance,
                 utils.number_density_to_ppm(n, coord.z),
+                marker="o",
                 label=name,
             )
-            p1.append(p)
-        r = np.linspace(coord.distance.min(), coord.distance.max(), 200)
-        x = np.linspace(coord.x.min(),   coord.x.max(), 200)
-        z = np.linspace(coord.z.min(),   coord.z.max(), 200)
-        C = self.plume_model.calc(x, 0, z, time=self.time)
-        p2 = ax2.plot(r, C / C.max(), c="darkgrey", label="plume-coeff.")
         ax1.set_xlabel("distance [m]")
         ax1.set_ylabel("concentration [ppm]")
-        ax2.set_ylabel("coefficient(normalized)")
         ax1.set_xlim(coord.x.min(), coord.x.max())
         ax1.set_ylim(0, None)
-        ax2.set_ylim(0, 1)
-        ax1.legend(handles=[*p1, *p2])
+        ax1.legend()
 
         # ax3.view_init(elev=20, azim=-155)
         y = np.linspace(-coord.x.max() / 2, coord.x.max() / 2, 200)
@@ -167,8 +200,4 @@ class PlumeEnvironment:
         fig.colorbar(im, label="Concentration [units]")
         ax3.set_xlabel("X axis")
         ax3.set_ylabel("Y axis")
-        # ax1.set_ylim(0, None)
-        # ax2.set_ylim(0, ax3.get_zlim()[1])
-        plt.tight_layout()
         plt.show(block=False)
-        # input("ENTER ANY KEY...")
